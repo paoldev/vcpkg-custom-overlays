efaae3b8219e6104bfadb5621f33987d8820b54b
paoldev <55021433+paoldev@users.noreply.github.com>
2023-06-06 17:03:21 +0200

    [000000-my-tools] Port to download some tools

---
+++ ports/000000-my-tools/portfile.cmake
@@ -0,0 +1,11 @@
+set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
+
+#vcpkg_find_acquire_program(CMAKE)
+#vcpkg_find_acquire_program(MESON)
+vcpkg_find_acquire_program(PERL)
+vcpkg_find_acquire_program(FLEX)
+vcpkg_find_acquire_program(RUBY)
+vcpkg_find_acquire_program(BISON)
+vcpkg_find_acquire_program(GPERF)
+vcpkg_find_acquire_program(PYTHON2)
+vcpkg_find_acquire_program(PYTHON3)
