0e28ba21faabea5725247a223152338cf613934d
paoldev <55021433+paoldev@users.noreply.github.com>
2026-01-08 12:39:15 +0100

    [tokenizer] Update to 1.38.0.

--- ports/tokenizer/portfile.cmake
+++ ports/tokenizer/portfile.cmake
@@ -3,7 +3,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OpenNMT/Tokenizer
     REF v${VERSION}
-    SHA512 ddda4853449aa2394c3da5abacbdb2121b5a728eaadf8dee2bd88618b3583cedb97b63a79b5d8e0e4904e1fe2ed0db6dc128d72c000f09885bb563b899880265
+    SHA512 cb3b7ab40aff37fabcba44f8a317d0ae1db541b7ed0815bcb4840f8f17e3a93b331632ce2313261992fc8a331c49c0ceb818dab59bad73b75c1ed950d056b5c9
     HEAD_REF master
 	PATCHES
 		fix-sentencepiece-dependency.patch
