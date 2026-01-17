5711d3c8b9893d2f046c65d1b6cfaf0496cb92a8
paoldev <55021433+paoldev@users.noreply.github.com>
2026-01-17 15:16:43 +0100

    [tokenizer] Update to 1.38.1.

--- ports/tokenizer/portfile.cmake
+++ ports/tokenizer/portfile.cmake
@@ -3,10 +3,10 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO OpenNMT/Tokenizer
     REF v${VERSION}
-    SHA512 cb3b7ab40aff37fabcba44f8a317d0ae1db541b7ed0815bcb4840f8f17e3a93b331632ce2313261992fc8a331c49c0ceb818dab59bad73b75c1ed950d056b5c9
+    SHA512 abae15a571721cb64f32b1abb178c65d793b38eebc69fbc0e31f36a42efd89ecaafc0ce262627a64fdfe6a3f59c779a0c084dc4df786878a8c699c3a030bafad
     HEAD_REF master
-	PATCHES
-		fix-sentencepiece-dependency.patch
+    PATCHES
+        fix-sentencepiece-dependency.patch
 )
 
 # Needed to find sentencepiece dependencies.
@@ -18,9 +18,9 @@ string(COMPARE EQUAL "${VCPKG_LIBRARY_LINKAGE}" "dynamic" BUILD_SHARED_LIBS)
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
-		-DLIB_ONLY=ON
+        -DLIB_ONLY=ON
         -DBUILD_TESTS=OFF
-		-DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
+        -DBUILD_SHARED_LIBS=${BUILD_SHARED_LIBS}
 )
 
 vcpkg_cmake_install()
