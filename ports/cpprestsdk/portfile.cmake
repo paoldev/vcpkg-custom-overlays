2e23aee37b24409122ecf7308fa2e4d1efc348f3
paoldev <55021433+paoldev@users.noreply.github.com>
2026-05-23 12:02:53 +0200

    [cpprestsdk] Update to 2.10.19#5.

--- ports/cpprestsdk/portfile.cmake
+++ ports/cpprestsdk/portfile.cmake
@@ -11,7 +11,8 @@ vcpkg_from_github(
         fix-clang-dllimport.patch # workaround for https://github.com/microsoft/cpprestsdk/issues/1710
         fix-asio-error.patch
         remove-stdext-checked-array-iterator-1836.patch # https://github.com/microsoft/cpprestsdk/pull/1836
-        my-windows-asio-unresolved-external-fix.patch #my-change: https://github.com/microsoft/cpprestsdk/pull/1466
+        remove-openprot-1844.diff # https://github.com/microsoft/cpprestsdk/pull/1844
+        my-windows-asio-unresolved-external-fix.patch # my-change: https://github.com/microsoft/cpprestsdk/pull/1466
 )
 
 vcpkg_check_features(
